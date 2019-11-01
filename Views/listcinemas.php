6cba69dec9d5443e3b0c9a4772849434f8c9fd20
ChristianSoldano <you@example.com>
2019-11-01 16:12:56 -0300

    Actualizado listCinema con imports en header y footer

--- Views/listcinemas.php
+++ Views/listcinemas.php
@@ -3,10 +3,6 @@
 <?php include_once(VIEWS_PATH . "header.php"); ?>
 
 <body>
-    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
-
-
-
 
     <?php
     include_once(VIEWS_PATH . "navAdmin.php");
@@ -51,7 +47,7 @@
                                 echo $i . ") " . $pelicula->getTitle() . "<br>";
                                 $i++;
                             }
-                            ?></td>
+                            ?>
                     </td>
                     <td><?php echo "$" . $cine->getTicketPrice(); ?></td>
                     <td><?php
@@ -63,84 +59,81 @@
                         <?php }
                             ?>
                     </td>
-                    <td>
-                    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#cinemaModify<?php echo $cine->getId(); ?>">Modificar</button>                    </td>
+                    <td> <a href="#cinemaModify<?php echo $cine->getId(); ?>" class="btn btn-info" data-toggle="modal">Modificar</a></td>
                 </tr>
             <?php } ?>
         </tbody>
     </table>
 
-    <?php include_once(VIEWS_PATH . "footer.php"); ?>
+    <?php
+    foreach ($listado as $cine) { ?>
+
+        <!-- MODAL -->
+        <div class="modal fade" id="cinemaModify<?php echo $cine->getId(); ?>">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <!-- HEADER DE LA VENTANA -->
+                    <div class="modal-header">
+                        <h2 class="modal-title"><?php echo "Modificar Cine: " . $cine->getName(); ?></h2>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <!-- CONTENIDO DE LA VENTANA -->
+                    <div class="modal-body">
+
+                        <form action="<?php echo FRONT_ROOT ?>Cinema/modifyCinema" method="post">
+                            <div class="form-group">
+                                <label for="id">ID</label>
+                                <input class="form-control" type="text" id="id" name="id" value="<?php echo $cine->getId(); ?>" readonly>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name">Nombre</label>
+                                <input class="form-control" type="text" id="name" name="name" placeholder="Nombre del Cine" value="<?php echo $cine->getName(); ?>" required pattern="[A-Za-z0-9 ]{1,40}" title="Solo letras o números (máximo 40 caracteres)">
+                            </div>
 
 
+                            <div class="form-group">
+                                <label for="address">Dirección</label>
+                                <input class="form-control" type="text" id="address" name="address" placeholder="Direccion del Cine" value="<?php echo $cine->getAddress(); ?>" required pattern="[A-Za-z0-9 ]{1,40}" title="Solo letras o números (máximo 40 caracteres)">
+                            </div>
 
-    <!-- MODAL -->
-    <div class="modal fade" id="cinemaModify<?php echo $cine->getId(); ?>">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
 
-                    <h4 class="modal-title"><?php echo "Modificar Cine: " . $cine->getName(); ?></h4>
-                    <button tyle="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <div class="container fluid">
-                        <div class="row">
+                            <div class="form-group">
+                                <label for="seats">Nº Asientos</label>
+                                <input id="seats" name="seats" placeholder="Cantidad de Asientos" type="number" required="required" value="<?php echo count($cine->getSeats()); ?>" class="form-control" min=1 max=50 title="Solo números entre 1 y 50">
+                            </div>
 
-                            <form action="<?php echo FRONT_ROOT ?>Cinema/modifyCinema" method="post">
-                                <div class="form-group">
-                                    <label for="id">
-                                        <h5>ID</h5>
-                                    </label>
-                                    <input class="form-control" type="text" id="id" name="id" value="<?php echo $cine->getId(); ?>" readonly>
-                                </div>
-                                <div class="form-group">
-                                    <label for="name">
-                                        <h5>Nombre</h5>
-                                    </label>
-                                    <input class="form-control" type="text" id="name" name="name" placeholder="Nombre del Cine" value="<?php echo $cine->getName(); ?>" required pattern="[A-Za-z0-9 ]{1,40}" title="Solo letras o números (máximo 40 caracteres)">
-                                </div>
-                                <div class="form-group">
-                                    <label for="address">
-                                        <h5>Dirección</h5>
-                                    </label>
-                                    <input class="form-control" type="text" id="address" name="address" placeholder="Direccion del Cine" value="<?php echo $cine->getAddress(); ?>" required pattern="[A-Za-z0-9 ]{1,40}" title="Solo letras o números (máximo 40 caracteres)">
-                                </div>
-                                <div class="form-group">
-                                    <label for="seats">
-                                        <h5>Nº Asientos</h5>
-                                    </label>
-                                    <input id="seats" name="seats" placeholder="Cantidad de Asientos" type="number" required="required" value="<?php echo count($cine->getSeats()); ?>" class="form-control" min=1 max=50 title="Solo números entre 1 y 50">
-                                </div>
-                                <div class="form-group">
-                                    <label for="price">
-                                        <h5>Precio</h5>
-                                    </label>
-                                    <input id="price" name="price" placeholder="Valor de la Entrada" type="number" required="required" value="<?php echo $cine->getTicketPrice(); ?>" class="form-control" min=1 max=3000 title="Solo números entre 1 y 3000">
-                                </div>
-                                <div class="form-group">
-                                    <label for="status">
-                                        <h5>Estado</h5>
-                                    </label><br>
-                                    <input type="checkbox" name="status" data-toggle="toggle" data-on="Activo" data-off="Inactivo" data-onstyle="success" data-offstyle="danger" data-width="100" <?php if($cine->getStatus() == true) echo "checked"; ?>>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th></th>
-                                                <th scope="col">Id</th>
-                                                <th scope="col">Nombre</th>
-                                                <th scope="col">Generos</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
 
-                                            <?php $arrayPeliculas = $cine->getBillBoard();
+                            <div class="form-group">
+                                <label for="price">Precio</label>
+                                <input id="price" name="price" placeholder="Valor de la Entrada" type="number" required="required" value="<?php echo $cine->getTicketPrice(); ?>" class="form-control" min=1 max=3000 title="Solo números entre 1 y 3000">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="status">Estado</label><br>
+                                <input type="checkbox" name="status" data-toggle="toggle" data-on="Activo" data-off="Inactivo" data-onstyle="success" data-offstyle="danger" data-width="100" <?php if ($cine->getStatus() == true) echo "checked"; ?>>
+                            </div>
+
+                            <div class="form-group">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th scope="col">Id</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Generos</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <?php $arrayPeliculas = $cine->getBillBoard();
                                             foreach ($nowPlaying as $val) {  ?>
-                                                <tr>
-                                                    <?php
+                                            <tr>
+                                                <?php
                                                         $validacion = false;
 
                                                         foreach ($arrayPeliculas as $pelicula) {
@@ -150,11 +143,11 @@
                                                             }
                                                         }
                                                         ?>
-                                                    <th scope="col"><input type="checkbox" name="moviechecked[]" value="<?php echo $val->getTitle(); ?>" <?php if ($validacion) echo "checked"; ?> /></th>
+                                                <th scope="col"><input type="checkbox" name="moviechecked[]" value="<?php echo $val->getTitle(); ?>" <?php if ($validacion) echo "checked"; ?> /></th>
 
-                                                    <th scope="row"><?php echo $val->getIdMovie(); ?></th>
-                                                    <td><?php echo $val->getTitle(); ?></td>
-                                                    <td><?php
+                                                <th scope="row"><?php echo $val->getIdMovie(); ?></th>
+                                                <td><?php echo $val->getTitle(); ?></td>
+                                                <td><?php
                                                             $generosPelicula = $val->getIdGenre();
                                                             foreach ($generosPelicula as $genero) {
                                                                 foreach ($arrayGeneros as $gen) {
@@ -164,20 +157,28 @@
                                                                 }
                                                             }
                                                             ?>
-                                                    </td>
-                                                </tr>
-                                        </tbody>
-                                    <?php }  ?>
-                                    </table>
-                                </div>
-                                <div class="form-group">
-                                    <button name="submit" type="submit" class="btn btn-primary btn-success btn-block">Modificar</button>
-                                </div>
-                        </div>
+                                                </td>
+                                            </tr>
+                                    </tbody>
+                                <?php }  ?>
+                                </table>
+                            </div>
                     </div>
+
+                    <!-- FOOTER DE LA VENTANA -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button name="submit" type="submit" class="btn btn-primary btn-success btn-block">Guardar Cambios</button>
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
+    <?php
+    }
 
-    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
+
+
+
+
+    include_once(VIEWS_PATH . "footer.php"); ?>
