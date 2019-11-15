d801e83c49a465bfdda3e87e163009f28f4824f4
ChristianSoldano <you@example.com>
2019-11-15 16:30:25 -0300

    modificado vistas de login y sign up

--- Views/login.php
+++ Views/login.php
@@ -1,27 +1,69 @@
-<body class="home">
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo FRONT_CSS; ?>estilos.css">
+    <script src="https://kit.fontawesome.com/21dd680c2a.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo FRONT_CSS; ?>style.css">
+    <title>MoviePass</title>
+</head>
+
+
+
 
 <?php
 
-    include_once(VIEWS_PATH . "header.php");
-    include_once(VIEWS_PATH . "nav.php");
+include_once(VIEWS_PATH . "nav.php");
 ?>
 
-<html>
-<br>
-<br>
-<form id="login" action="<?php echo FRONT_ROOT ?>User/logIn" method="POST">
-    <div class="container">
-        <input type="text" name="user" id="name" placeholder="User" required="required">
-        <input type="password" name="password" id="password" placeholder="Password" required="required">
-        <button type="submit" id="login-submit">LOGIN</button> 
-    </div>
-</form>
+<body class="home">
+<br><br><br><br>
+    
+        <!-- Sing in  Form -->
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="<?php echo FRONT_ROOT; ?>Views/img/logo.png" alt="sing up image"></figure>
+                        <a href="<?php echo FRONT_ROOT?>User/signUpForm" class="signup-image-link">Crear Cuenta</a>
+                    </div>
 
-<br>
-<a class="home-link" href=<?php echo FRONT_ROOT?>>Home</a>
+                    <div class="signin-form">
+                        <h2 class="form-title">Iniciar Sesión</h2>
+                        <form action="<?php echo FRONT_ROOT ?>User/logIn" method="post" class="register-form" id="login-form">
+                            <div class="form-group">
+                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="user" id="name" placeholder="Usuario" required="required" />
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Contraseña" required="required" />
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Recordarme</label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Ingresar" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+ 
 
-</html>
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
 
-<?php
-include_once(VIEWS_PATH . "footer.php");
-
+    <?php
+    include_once(VIEWS_PATH . "footer.php");
--- Views/signup.php
+++ Views/signup.php
@@ -1,31 +1,67 @@
-<body class="home">
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo FRONT_CSS; ?>estilos.css">
+    <script src="https://kit.fontawesome.com/21dd680c2a.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo FRONT_CSS; ?>style.css">
+    <title>MoviePass</title>
+</head>
 
 <?php
-
-    include_once(VIEWS_PATH . "header.php");
-    include_once(VIEWS_PATH . "nav.php");
-
+include_once(VIEWS_PATH . "nav.php");
 ?>
+<br><br><br><br>
 
-<html>
+<body class="home">
+    <!-- Sign up form -->
+    <section class="signup">
+        <div class="container">
+            <div class="signup-content">
+                <div class="signup-form">
+                    <h2 class="form-title">Registrarse</h2>
+                    <form action="<?php echo FRONT_ROOT ?>User/signUp" method="post" class="register-form" id="register-form">
+                        <div class="form-group">
+                            <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                            <input type="text" name="user" id="name" placeholder="Usuario" required pattern="[A-Za-z0-9]{1,15}" title="Solo letras o números (máximo 15 caracteres)" />
+                        </div>
+                        <div class="form-group">
+                            <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                            <input type="password" name="password" id="password" placeholder="Contraseña" required pattern="[A-Za-z0-9]{4,}" title="Solo letras o números (mínimo 4 caracteres)" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email"><i class="zmdi zmdi-email"></i></label>
+                            <input type="email" name="email" id="email" placeholder="Email" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="firstname"><i class="zmdi zmdi-email"></i></label>
+                            <input type="text" name="firstname" id="firstname" placeholder="Nombre" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname"><i class="zmdi zmdi-email"></i></label>
+                            <input type="text" name="lastname" id="lastname" placeholder="Apellido" required>
+                        </div>
+                        <div class="form-group form-button">
+                            <input type="submit" name="signup" id="signup" class="form-submit" value="Registrarse" />
+                        </div>
+                    </form>
+                </div>
+                <div class="signup-image">
+                    <figure><img src="<?php echo FRONT_ROOT; ?>Views/img/logo.png" alt="sing up image"></figure>
+                    <a href="<?php echo FRONT_ROOT ?>User/logInForm" class="signup-image-link">Ya estoy registrado</a>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<br>
-<br>
-<form id="signup" action="<?php echo FRONT_ROOT ?>User/signUp" method="POST">
-    <input type="text" name="user" id="name" placeholder="User" required pattern="[A-Za-z0-9]{1,15}" title="Solo letras o números (máximo 15 caracteres)">
-    <input type="password" name="password" id="password" placeholder="Password" required pattern="[A-Za-z0-9]{4,}" title="Solo letras o números (mínimo 4 caracteres)">
-    <input type="text" name="firstname" id="firstname" placeholder="First Name" required >
-    <input type="text" name="lastname" id="lastname" placeholder="Last Name" required >
-    <input type="email" name="email" id="email" placeholder="Email" required>
-    <input type="number" name="dni" id="dni" placeholder="DNI" required pattern="[0-9]{8}" title="Solo números (máximo 8 caracteres)">
-    <button type="submit" id="login-submit">SIGN UP</button>
-</form>
+    <?php
 
-<br>
-<a class="home-link" href=<?php echo FRONT_ROOT?>>Home</a>
-
-</html>
-
-<?php
-
-include_once(VIEWS_PATH . "footer.php");
+    include_once(VIEWS_PATH . "footer.php");
